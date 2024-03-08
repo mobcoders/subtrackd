4ed82013e1c0c38d6b867bc119de771f14bcc4e7
Anna Ewbank <Ewbank145@gmail.com>
2024-03-08 10:55:21 +0000

    refactor: add dotenv import

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,11 @@
 import { job } from './scheduledTasks/subscriptionChecker';
 
-
-require('./scheduledTasks/subscriptionChecker')
-
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors'; 
-import {router} from './router';
+import cors from 'cors';
+import { router } from './router';
+import dotenv from 'dotenv'; // Import dotenv
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 
@@ -14,15 +13,15 @@ app.use(cors());
 app.use(express.json());
 app.use('/', router);
 
-
-//MongoDB connection 
-mongoose.connect(process.env.MONGODB_URI)
-.then(()=> console.log('MongoDB connected'))
-.catch((err: Error) => console.error('Could not connect to MongoDB...', err));
+//MongoDB connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err: Error) => console.error('Could not connect to MongoDB...', err));
 
 job.start();
 
 const port = process.env.port || 3000;
-app.listen(port, ()=>{
- console.log(`Server running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
