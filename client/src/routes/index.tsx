bfb3d77fb3c55efa5a015992dc991c2d1af2fb4d
Anna Ewbank <Ewbank145@gmail.com>
2024-03-13 17:29:12 +0000

    style: homepage css

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -6,6 +6,20 @@ import { Link, useNavigate } from 'react-router-dom';
 export default function IndexPage() {
   const { userId } = useAuth();
   const navigate = useNavigate();
+  const info = [
+    {
+      title: 'Real-time tracking',
+      text: 'View and organize all your subscriptions in one place.',
+    },
+    {
+      title: 'Daily, monthly, and yearly totals',
+      text: 'Understand your spending at a glance.',
+    },
+    {
+      title: 'Payment notifications',
+      text: 'Stay ahead with timely reminders.',
+    },
+  ];
 
   useEffect(() => {
     if (userId) {
@@ -15,43 +29,43 @@ export default function IndexPage() {
   return (
     <div className="container mx-auto px-4 col-span-12">
       <div className="text-center my-10">
-        <h1 className="text-5xl font-bold">Welcome to SubTrackd</h1>
-        <p className="text-xl mt-4">Your Ultimate Subscription Management Tool!</p>
+        <h1 className="text-6xl text-blush">
+          <span className="font-bold text-white">sub</span>
+          <span className="font-bold text-pink">trackd</span>
+        </h1>
+        <p className="text-3xl mt-4 text-blush">
+          The ultimate subscription management tool.
+        </p>
       </div>
 
-      <div className="my-6">
-        <p className="text-center text-lg">Never Lose Track of Your Subscriptions Again!</p>
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-8">
-        <Card isHoverable isPressable>
-          <CardBody>
-            <p className="text-lg font-semibold">Real-Time Tracking</p>
-            <p>View and organize all your subscriptions in one place.</p>
-          </CardBody>
-        </Card>
-
-        <Card isHoverable isPressable>
-          <CardBody>
-            <p className="text-lg font-semibold">Daily, Monthly, and Yearly Totals</p>
-            <p>Understand your spending at a glance.</p>
-          </CardBody>
-        </Card>
-
-        <Card isHoverable isPressable>
-          <CardBody>
-            <p className="text-lg font-semibold">Next Billing Cycle Notifications</p>
-            <p>Stay ahead with timely reminders.</p>
-          </CardBody>
-        </Card>
+      <div className="grid grid-cols-3 gap-4 my-8 mx-48">
+        {info.map((information, index) => (
+          <Card className="bg-transparent text-white border-solid border-2 border-pink">
+            <CardBody className="text-center">
+              <p className="text-xl font-semibold mb-3">{information.title}</p>
+              <p>{information.text}</p>
+            </CardBody>
+          </Card>
+        ))}
       </div>
 
       <div className="text-center my-10">
-        <h4 className="text-2xl font-bold">Why SubTrackd?</h4>
-        <p className="mt-4">Join SubTrackd today and take control of your subscriptions.</p>
-        <Link to="/sign-in"><Button className='mt-6 mx-3'>Sign in</Button></Link>
-        <Link to="/sign-up"><Button className='mt-6 mx-3'>Sign up</Button></Link>
-
+        <p className="mt-4 text-2xl ">
+          Join today to take control of your subscriptions.
+        </p>
+        <Link to="/sign-in">
+          <Button className="mt-6 mx-3 bg-pink text-white" size="lg">
+            Sign in
+          </Button>
+        </Link>
+        <Link to="/sign-up">
+          <Button
+            className="mt-6 mx-3 bg-transparent border-solid border-2 border-pink text-white hover:bg-pink"
+            size="lg"
+          >
+            Sign up
+          </Button>
+        </Link>
       </div>
     </div>
   );
