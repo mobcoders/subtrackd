121da1937315a1c44508e70520c0ac6ff570f4b2
Arjun Gill <arjungill6ep@gmail.com>
2024-03-13 15:04:44 +0000

    feat: authentication front end

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,17 +1,29 @@
-import { Image } from '@nextui-org/react';
+import { Button, Image } from '@nextui-org/react';
 import Notifications from './notifications';
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
+import { Link } from 'react-router-dom';
 
 // RENDER:
 export default function Navbar() {
   return (
     <div className="col-span-12 flex justify-between my-12">
+      <Link to="/">
       <Image
         width={150}
         alt="Subtrackd Logo"
         src="/subtrackd-logo.svg"
-        className="rounded-none"
+        className="rounded-none cursor-pointer"
       />
-      <Notifications />
+      </Link>
+      <SignedIn>
+        <div className='flex gap-3'>
+          <Notifications />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <Link to="/sign-in"><Button>Sign in</Button></Link>
+      </SignedOut>
     </div>
   );
 }
