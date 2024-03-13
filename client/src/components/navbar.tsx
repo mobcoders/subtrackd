8b9c758dfdf16aaeea06adba68cd80fadcf3f5fe
Anna Ewbank <Ewbank145@gmail.com>
2024-03-13 18:25:45 +0000

    style: homepage css

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -6,17 +6,32 @@ import { Link } from 'react-router-dom';
 // RENDER:
 export default function Navbar() {
   return (
-    <div className="col-span-12 flex justify-between my-12">
+    <div className="col-span-12 flex justify-between mt-12">
       <Link to="/">
-      <Image
-        width={150}
-        alt="Subtrackd Logo"
-        src="/subtrackd-logo.svg"
-        className="rounded-none cursor-pointer"
-      />
+        <Image
+          width={150}
+          alt="Subtrackd Logo"
+          src="/subtrackd-logo.svg"
+          className="rounded-none cursor-pointer"
+        />
       </Link>
+      <div>
+        <Link to="/sign-in">
+          <Button className="mt-6 mx-3 bg-pink text-white" size="lg">
+            Sign in
+          </Button>
+        </Link>
+        <Link to="/sign-up">
+          <Button
+            className="mt-6 mx-3 bg-transparent border-solid border-2 border-pink text-white hover:bg-pink"
+            size="lg"
+          >
+            Sign up
+          </Button>
+        </Link>
+      </div>
       <SignedIn>
-        <div className='flex gap-3'>
+        <div className="flex gap-3">
           <Notifications />
           <UserButton afterSignOutUrl="/sign-in" />
         </div>
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from '@clerk/clerk-react';
-import { Button, Card, CardBody } from '@nextui-org/react';
+import { Button, Card, CardBody, Image } from '@nextui-org/react';
 import { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -21,51 +21,50 @@ export default function IndexPage() {
     },
   ];
 
+  // USE EFFECT:
   useEffect(() => {
     if (userId) {
       navigate('/dashboard');
     }
   });
+
+  // RENDER:
   return (
-    <div className="container mx-auto px-4 col-span-12">
-      <div className="text-center my-10">
-        <h1 className="text-6xl text-blush">
+    <div className="mx-auto px-4 col-span-12">
+      <div className="text-center">
+        <h1 className="text-6xl text-blush mb-4">
           <span className="font-bold text-white">sub</span>
           <span className="font-bold text-pink">trackd</span>
         </h1>
-        <p className="text-3xl mt-4 text-blush">
+        <p className="text-3xl mb-10 text-blush">
           The ultimate subscription management tool.
         </p>
       </div>
 
-      <div className="grid grid-cols-3 gap-4 my-8 mx-48">
-        {info.map((information, index) => (
-          <Card className="bg-transparent text-white border-solid border-2 border-pink">
-            <CardBody className="text-center">
-              <p className="text-xl font-semibold mb-3">{information.title}</p>
-              <p>{information.text}</p>
-            </CardBody>
-          </Card>
-        ))}
-      </div>
-
-      <div className="text-center my-10">
-        <p className="mt-4 text-2xl ">
-          Join today to take control of your subscriptions.
-        </p>
-        <Link to="/sign-in">
-          <Button className="mt-6 mx-3 bg-pink text-white" size="lg">
-            Sign in
-          </Button>
-        </Link>
-        <Link to="/sign-up">
-          <Button
-            className="mt-6 mx-3 bg-transparent border-solid border-2 border-pink text-white hover:bg-pink"
-            size="lg"
-          >
-            Sign up
-          </Button>
-        </Link>
+      <div className="flex gap-10 mb-5">
+        <Image
+          width={600}
+          alt="subtrackd demo"
+          src="/subtrackd-demo.png"
+          className="p-0 m-0"
+        />
+        <div className="flex flex-col gap-5 w-80 justify-center items-center">
+          {info.map((information, index) => (
+            <Card className="bg-transparent text-white border-solid border-2 border-pink">
+              <CardBody className="text-center">
+                <p className="text-xl font-semibold mb-3">
+                  {information.title}
+                </p>
+                <p>{information.text}</p>
+              </CardBody>
+            </Card>
+          ))}
+          <Link to="/sign-up">
+            <Button className="bg-pink text-white" size="lg">
+              Join today
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
