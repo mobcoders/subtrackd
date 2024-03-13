8ae895269e9fe81a003d0d17055d0a73be51c427
Arjun Gill <arjungill6ep@gmail.com>
2024-03-13 11:57:53 +0000

    style: insights

--- client/src/pages/dashboard/insights.tsx
+++ client/src/pages/dashboard/insights.tsx
@@ -1,45 +1,51 @@
-import { Card, CardHeader, CardBody, Divider } from '@nextui-org/react';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Divider,
+  Tab,
+  Tabs,
+} from '@nextui-org/react';
 import { useStore } from '../../zustand/store';
 
 export default function Insights() {
-
   //ZUSTAND
   const monthlyTotal = useStore((state) => state.monthlyTotal).toFixed(2);
- 
+
   //Calculations
-  const dailyTotal = (parseFloat(monthlyTotal)/30).toFixed(2);
+  const dailyTotal = (parseFloat(monthlyTotal) / 30).toFixed(2);
   const yearlyTotal = (parseFloat(monthlyTotal) * 12).toFixed(2);
 
   //RENDER
   return (
     <Card
-      isBlurred
       shadow="sm"
-      className="col-span-4 border-none text-white bg-background/60 bg-transparent"
+      className="col-span-4 text-white border-solid border-2 border-pink bg-transparent"
     >
       <CardHeader className="flex justify-center">
         <p>Insights</p>
       </CardHeader>
       <Divider />
-      <CardBody className="flex flex-col justify-around">
-        <div className="text-center">
-          <p>
-            <strong>Daily</strong>
-          </p>
-          <p>£{dailyTotal}</p>
-        </div>
-        <div className="text-center">
-          <p>
-            <strong>Monthly</strong>
-          </p>
-          <p>£{monthlyTotal}</p>
-        </div>
-        <div className="text-center">
-          <p>
-            <strong>Yearly</strong>
-          </p>
-          <p>£{yearlyTotal}</p>
-        </div>
+      <CardBody className='flex flex-col items-center'>
+        <Tabs aria-label="Options">
+          <Tab key="daily" title="Daily">
+            <Card>
+              <CardBody className='text-center'>£{dailyTotal}</CardBody>
+            </Card>
+          </Tab>
+          <Tab key="monthly" title="Monthly">
+            <Card>
+              <CardBody className='text-center'>£{monthlyTotal} </CardBody>
+            </Card>
+          </Tab>
+          <Tab key="yearly" title="Yearly">
+            <Card>
+              <CardBody className='text-center'>
+              £{yearlyTotal}
+              </CardBody>
+            </Card>
+          </Tab>
+        </Tabs>
       </CardBody>
       <Divider />
     </Card>
